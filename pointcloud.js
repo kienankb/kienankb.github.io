846e37543fc4fdefcfc89811a4bcc349715ff661
Kienan Knight-Boehm <kienan@kienankb.com>
2019-08-24 23:33:19 -0700

    Added pointcloud testing

---
+++ pointcloud.js
@@ -0,0 +1,30 @@
+let WINDOW_WIDTH = 640;
+let WINDOW_HEIGHT = 480;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	draw();
+});
+
+class Point {
+	constructor() {
+		this.x = Math.random() * WINDOW_WIDTH;
+		this.y = Math.random() * WINDOW_HEIGHT;
+		this.v = Math.random() * 360;
+	}
+}
+
+function draw() {
+	var elem = document.getElementById('draw-shapes');
+	var two = new Two({ width: WINDOW_WIDTH, height: WINDOW_HEIGHT }).appendTo(elem);
+	let points = [];
+	for (var i = 0; i < 5; i++) {
+		points.push(new Point());
+	}
+	console.log(points);
+	two.bind('update', function(frameCount) {
+		for (var i = 0; i < points.length; i++) {
+			let dot = two.makeCircle(points[i].x, points[i].y, 5);
+			dot.fill = 'red';
+		}
+	}).play();
+}
