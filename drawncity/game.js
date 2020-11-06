553bdf191c4bd4b48c96ba25ab33bc725374e624
Kienan Knight-Boehm <kienan@kienankb.com>
2020-11-05 23:18:17 -0800

    Add a few things, condense material/map work, fix orientation

--- drawncity/game.js
+++ drawncity/game.js
@@ -18,19 +18,28 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    var groundMap = new THREE.TextureLoader().load( "city_game_tileset/ground_tile_porous1.png" );
+    let pathPrefix = "city_game_tileset/";
+
+    let groundMaterial = new THREE.SpriteMaterial({
+        map: new THREE.TextureLoader().load(`${pathPrefix}ground_tile_porous1.png`)});
+
+    // var groundMap = new THREE.TextureLoader().load( "city_game_tileset/ground_tile_porous1.png" );
     var phoneMap = new THREE.TextureLoader().load( "city_game_tileset/telephone-booth1.png" );
     var trashMap = new THREE.TextureLoader().load( "city_game_tileset/manhole1.png" );
     var barrierMap = new THREE.TextureLoader().load( "city_game_tileset/barrier1.png" );
-    var groundMaterial = new THREE.SpriteMaterial( { map: groundMap } );
+    var ghostMap = new THREE.TextureLoader().load( "city_game_tileset/ghost1.png" );
+    var personMap = new THREE.TextureLoader().load( "city_game_tileset/palm2.png" );
+    // var groundMaterial = new THREE.SpriteMaterial( { map: groundMap } );
     var phoneMaterial = new THREE.SpriteMaterial( { map: phoneMap } );
     var trashMaterial = new THREE.SpriteMaterial( { map: trashMap } );
     var barrierMaterial = new THREE.SpriteMaterial( { map: barrierMap } );
+    var ghostMaterial = new THREE.SpriteMaterial( { map: ghostMap } );
+    var personMaterial = new THREE.SpriteMaterial( { map: personMap } );
 
     let grid = [
         [[groundMaterial, trashMaterial], [groundMaterial], [groundMaterial, phoneMaterial]],
         [[groundMaterial], [groundMaterial], [groundMaterial]],
-        [[groundMaterial], [groundMaterial], [groundMaterial, barrierMaterial]]
+        [[groundMaterial, personMaterial], [groundMaterial, ghostMaterial], [groundMaterial, barrierMaterial]]
     ];
 
     for (let i = 0; i < grid.length; i++) {
@@ -38,9 +47,8 @@ function init() {
             let stack = grid[i][j];
             stack.forEach((tile) => {
                 let sprite = new THREE.Sprite(tile);
-                // sprite.scale.set(3, 6.33, 1);
                 sprite.scale.set(3.55, 7.490, 1);
-                sprite.position.set(-i, 0, -j);
+                sprite.position.set(j, 0, i);
                 scene.add(sprite);
             });
         }
